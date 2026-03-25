157219188363cd2146fffb38c785d80976eee045
pizzaalano4 <pizzaalano@gmail.com>
2026-03-25 16:09:06 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
